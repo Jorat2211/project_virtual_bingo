77d69c9427bcee0a1a6904e47810b5d857f089e9
Gerardo <gera2espi1818@gmail.com>
2020-10-23 21:46:33 -0600

    searchs cards for show

--- RedCrossBingo/ClientApp/src/app/game/game.component.ts
+++ RedCrossBingo/ClientApp/src/app/game/game.component.ts
@@ -11,18 +11,31 @@ import {BingoCard} from './bingocards.interface';
 })
 export class GameComponent  {
   public roomsId = 2; 
-  public cards : BingoCard[]; 
+  public cards : Array<BingoCard>; 
+  public card : BingoCard; 
+  public number : BingoCardsNumbers; 
   constructor(public http: HttpClient, @Inject('BASE_URL') public baseUrl: string) {
+    this.getCards(); 
    }
 
 
 
+ paintNumbers(){
+   if(this.cards){
+      this.cards.forEach(element => {
+        console.log(element); 
+        this.card = element as BingoCard; 
+       this.card.bingoCardNumbers.forEach(e => {
+        
+       });
+      });
+   }
+ }  
+
 getCards(){
   console.log(this.baseUrl + 'api/Bingocards/'+this.roomsId);
     this.http.get<BingoCard[]>(this.baseUrl + 'api/Bingocards/'+this.roomsId).subscribe(result => {
-      this.cards = result;
-      console.log(this.cards); 
-
+      this.cards = result; 
     }, error => console.error(error));
 }
 
