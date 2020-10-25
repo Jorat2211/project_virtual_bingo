6cd9a8a4967fd9b1a33b3153f7a2f1e178b024b5
Pabl021 <josepablobc21@gmail.com>
2020-10-25 17:54:21 -0600

    update numbers true in the BD and get

--- RedCrossBingo/ClientApp/src/app/game/game.component.ts
+++ RedCrossBingo/ClientApp/src/app/game/game.component.ts
@@ -23,21 +23,21 @@ export class GameComponent  {
 
 
   constructor(public http: HttpClient, @Inject('BASE_URL') public baseUrl: string) {
-<<<<<<< HEAD
+
     this.getCards(); 
     this.newCardNumber();
+    this.numberChooseTrue = [];
+    this.getNumbersTrue();
     
-=======
-      this.getCards(); 
->>>>>>> e8c09af39c984858664b48f7a184e3fecd67202c
    }
 
   newCardNumber(){
     this.bingoNumber={
       id:-1,
       number: 0,
-      is_chosen: false,
-      rooms_id:-1
+      isChosen:false,
+      roomsId:-1
+     
     }
   }
 
@@ -67,20 +67,12 @@ newBingoNumber(){
   this.bingoNumber = {
   id:0,
   number: 0,
-
-  is_chosen:false,
-  rooms_id: 6 //Este id es default, debe cambiarse
-
+  isChosen:false,
+  roomsId:6 //Este id es default, debe cambiarse
   }
 }
 
 save(){
-  if(this.bingoNumber.id > 0){
-    //update
-    this.http.put<BingoNumber>(this.baseUrl +'api/Bingonumber/'+this.bingoNumber.id, this.bingoNumber).subscribe(result=>{
-    }, error=>console.error(error));
-    return;
-  }
   //Insert
      this.http.post<BingoNumber> (this.baseUrl + 'api/Bingonumber', {
       number : this.bingoNumber.number,
@@ -91,41 +83,56 @@ save(){
   }, error => console.error(error));
 }
 
-generateNumberTombola(){
-
-  
+generateNumberTombola(){  
   this.newBingoNumber();
   for (var i = 1; i < 76; i++)
   {  
     this.bingoNumber.number=i; 
     this.save();   
-  
-
   }
 }
 
 getNumber(){
   var numberRandom = this.newRandom();
-  console.log(numberRandom);
+  while(this.numberChooseTrue.includes(numberRandom)){
+    numberRandom= this.newRandom();
+  }
   if(!this.numberChooseTrue.includes(numberRandom)){
-    console.log("entro");
-    // this.numberChooseTrue.push(numberRandom);
-    // console.log(this.numberChooseTrue);
-    // this.http.get<BingoNumber>(this.baseUrl + 'api/Bingonumber/' + '6/' + numberRandom).subscribe(result => {
-    //   var bingoNumber = result as BingoNumber;
-    //   this.bingoNumber.number = bingoNumber.number;
-    // }, error => console.error(error));
+     this.numberChooseTrue.push(numberRandom);
+     console.log(this.numberChooseTrue);
+     this.http.get<BingoNumber>(this.baseUrl + 'api/Bingonumber/' + '6/' + numberRandom).subscribe(result => {
+       var bingoNumberResult = result as BingoNumber;
+       bingoNumberResult.isChosen=true;
+      this.updateNumber(bingoNumberResult);
+       this.bingoNumber.number = bingoNumberResult.number;
+     }, error => console.error(error));
   }
   
+}
+
+getNumbersTrue(){
+  this.http.get<BingoNumber[]>(this.baseUrl + 'api/Bingonumber/' + 'true/').subscribe(result => {
+    result.forEach(element => {
+      this.numberChooseTrue.push(element.number);
+    });
+  }, error => console.error(error));
 }
 
   newRandom() {
     const min = 1;
     const max = 75;
     return Math.floor(Math.random() * (max - min + 1) + min);
+    
   }
  
-
+updateNumber(bingoNumber:BingoNumber){
+  if(bingoNumber.id > 0){
+    //update
+    this.http.put<BingoNumber>(this.baseUrl +'api/Bingonumber/'+bingoNumber.id, bingoNumber).subscribe(result=>{
+    }, error=>console.error(error));
+    return;
+  }
+}
 
 
 }
