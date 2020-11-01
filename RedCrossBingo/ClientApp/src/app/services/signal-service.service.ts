06a330c2d6cb27f5c9712f1f57a61039082f8b25
Gerardo <gera2espi1818@gmail.com>
2020-11-01 08:14:17 -0600

    test

--- RedCrossBingo/ClientApp/src/app/services/signal-service.service.ts
+++ RedCrossBingo/ClientApp/src/app/services/signal-service.service.ts
@@ -9,9 +9,6 @@ import { BingoNumber} from './../game/bingonumbers.interface';
 export class SignalServiceService {
   //npm install @aspnet/signalr -->Install this package
   public hubConnection : HubConnection; 
-  public  url_bingoNumber : "https://localhost:5001/api/Bingonumber";
-  public  url_bingocardsnumber : "https://localhost:5001/api/Bingocardsnumber";
-
 
   eNotificarNumber : EventEmitter<BingoNumber> = new EventEmitter(); 
   eNoficUsers : EventEmitter<Number> = new EventEmitter(); 
@@ -19,18 +16,14 @@ export class SignalServiceService {
   
   constructor()  { 
     let builder = new HubConnectionBuilder(); 
-    //"https://localhost:5001/api/Bingonumber/-1"
     this.hubConnection = builder.withUrl("https://localhost:5001/api/Bingonumber/-1").build(); 
     this.hubConnection.on("SendBingoNumber", (msj) => {
        let bingo : BingoNumber = JSON.parse(msj); 
-       console.log("bingo: " +bingo); 
       this.eNotificarNumber.emit(bingo); 
     });
    
      this.hubConnection.on("SendNumbersPlayers",(msj)=>{
-       console.log("Conexion conetados"); 
        
-       console.log("Mensaje:  " +msj); 
       this.eNoficUsers.emit(msj); 
      });
     this.hubConnection.start().catch(err=>{
