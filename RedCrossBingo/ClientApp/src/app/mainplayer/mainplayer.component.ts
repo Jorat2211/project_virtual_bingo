9bb570787d9cf33694e7c04b1e34a6d830399b8c
Gerardo <gera2espi1818@gmail.com>
2020-11-03 14:39:43 -0600

    Internal documentation

--- RedCrossBingo/ClientApp/src/app/mainplayer/mainplayer.component.ts
+++ RedCrossBingo/ClientApp/src/app/mainplayer/mainplayer.component.ts
@@ -13,12 +13,12 @@ import { Router } from '@angular/router';
 })
 
 export class MainplayerComponent {
-  public cant : number; 
-  public card : BingoCard; 
-  public cardNumbers: BingoCardsNumbers; 
+  public cant : number;  //numbers of card bingo chosen 
+  public card : BingoCard; //card bingo
+  public cardNumbers: BingoCardsNumbers; //number for card 
 
-  private maxNumberCard = 0; 
-  private roomId: number;
+  private maxNumberCard = 0; //max number of card
+  private roomId: number; //id room
 
   constructor(public http: HttpClient, @Inject('BASE_URL') public baseUrl: string, private _route: ActivatedRoute,  private Router: Router) { 
     this.roomId = 0; 
@@ -28,6 +28,9 @@ export class MainplayerComponent {
     this.getCardsMax();
   }
 
+  /** 
+   * Create cards bingo
+   */
   createCards(){
     this.getCardsMax(); 
     let contador = this.maxNumberCard; 
@@ -37,16 +40,19 @@ export class MainplayerComponent {
       contador++; 
     }
     this.getCardsMax(); 
-    this.saveCardIdInSession(this.cant); 
     this.Router.navigate(['/Game/' + this._route.snapshot.paramMap.get('roomname')]); 
 }
-
+  /**
+   * Get max card
+   */
   getCardsMax(){
       this.http.get<number>(this.baseUrl + 'api/Bingocards/max/'+this.roomId).subscribe(result => {
         this.maxNumberCard = result+1; 
       }, error => console.error(error));
   }
-
+/**
+ * Initialize card
+ */
 newCard(){
   this.card = {
     id : 0,
@@ -55,7 +61,9 @@ newCard(){
     rooms_id : this.roomId
   }
 }
-
+/**
+ * Number of card
+ */
 newCardNumbers(){
   this.cardNumbers = {
     id : 0,
@@ -65,6 +73,10 @@ newCardNumbers(){
   }
 }
 
+/**
+ * save new card in the db, and also update card
+ * @param contador number of card bingo
+ */
 saveBingoCards(contador){
   if(this.card.id > 0){
     this.http.put<BingoCard>(this.baseUrl +'api/Bingocards'+this.card.id, this.card).subscribe(result=>{
@@ -84,10 +96,9 @@ saveBingoCards(contador){
   }, error => console.error(error));
 }
 
-saveCardIdInSession(cantidad: number){
-  sessionStorage.setItem("cantidad",JSON.stringify(cantidad)); 
-}
-
+/**
+ * Generate numbers for each card, numbers is ramdom
+ */
 generateNumbers(){
   let list = [];
   let num = this.getRamdon();
@@ -102,15 +113,18 @@ generateNumbers(){
   }
 }
 
+/**
+ * Get number of ramdon in range 1 to 75
+ */
 getRamdon(){
   const min=1;
   const max=75;
   return  Math.floor(Math.random()*(max-min+1)+min);
 }
 
-
-
-
+/**
+ * Save number in the db 
+ */
 saveBingoCardsNumbers(){
   if(this.cardNumbers.id > 0){
     this.http.put<BingoCard>(this.baseUrl +'api/Bingocardnumbers'+this.cardNumbers.id, this.cardNumbers).subscribe(result=>{
@@ -126,6 +140,10 @@ saveBingoCardsNumbers(){
   }, error => console.error(error));
 }
 
+/**
+ * save id the card in the session storage for to use
+ * @param id_card id of card
+ */
 saveIdCardInSessionStorage(id_card: number){
   let values = JSON.parse(sessionStorage.getItem("listCards"));
   let data ; 
@@ -141,6 +159,9 @@ saveIdCardInSessionStorage(id_card: number){
     sessionStorage.setItem("listCards", JSON.stringify(data)); 
 }
 
+/**
+ * Get id of room
+ */
 idRoom() {
   this.http.get<Room>(this.baseUrl + 'api/Bingocardnumbers/roomname/' + this._route.snapshot.paramMap.get('roomname')).subscribe(result => {
     this.roomId = Number(result);
--- RedCrossBingo/Hubs/BingoHub.cs
+++ RedCrossBingo/Hubs/BingoHub.cs
@@ -15,8 +15,7 @@ namespace RedCrossBingo.Hubs
             SendNumbersPlayers(); 
             return base.OnConnectedAsync();      
         }
-
-
+       
         public override Task OnDisconnectedAsync(Exception exception)
         {
             contador--; 
@@ -24,15 +23,25 @@ namespace RedCrossBingo.Hubs
             return base.OnDisconnectedAsync(exception);
         }
 
-
+        ///<sumary>
+        ///Send bingo for all clients
+        ///<param name= "bNumber"></param> number receive
+        ///</sumary>
         public Task SendBingoNumber (string bNumber){
             return Clients.All.SendAsync("bingoNumber", bNumber); 
         }
 
+        ///<sumary>
+        ///Send numbers of players to admin
+        ///</sumary>
         public Task SendNumbersPlayers (){
             return Clients.All.SendAsync("SendNumbersPlayers", contador); 
         }
 
+        ///<sumary>
+        ///Send mesagge is winner or lose
+        ///<param name= "isWinner"></param> 
+        ///</sumary>
         public Task SendMensageWinner(string isWinner){
             return Clients.All.SendAsync(" SendMensageWinner", isWinner); 
         }
