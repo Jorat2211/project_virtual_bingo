ca34c3759e396afefd28321b0dbc04a778297153
Pabl021 <josepablobc21@gmail.com>
2020-11-03 13:02:30 -0600

    documenting...

--- RedCrossBingo/ClientApp/src/app/game/game.component.ts
+++ RedCrossBingo/ClientApp/src/app/game/game.component.ts
@@ -170,7 +170,6 @@ updateCardNumber(number : BingoCardsNumbers){
     }
   }
 
-
   getNumbersTrue() {
     this.http.get<BingoNumber[]>(this.baseUrl + 'api/Bingonumber/' + 'true/').subscribe(result => {
       result.forEach(element => {
